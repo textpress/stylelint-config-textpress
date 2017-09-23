d6b0713cd9d2acf9046d3871ff44151ff18307f1
Aleksey Gurtovoy <agurtovoy@acm.org>
2017-09-22 22:26:26 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict"
+
+module.exports = {
+    "extends": "stylelint-config-recommended",
+    "rules": {
+        "indentation": 4,
+        "font-family-name-quotes": "always-unless-keyword",
+        "function-url-quotes": "always"
+    }
+};
